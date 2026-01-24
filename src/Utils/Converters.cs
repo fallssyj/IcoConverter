901e130c8592a6caf2af2f6c3e4bfde8622851bb
syj <falls_syj@qq.com>
2026-01-24 14:29:57 +0800

    Update Converters.cs

--- src/Utils/Converters.cs
+++ src/Utils/Converters.cs
@@ -1,4 +1,4 @@
-using IcoConverter.ViewModels;
+using IcoConverter.Models;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
@@ -6,26 +6,27 @@ using System.Windows.Data;
 
 namespace IcoConverter.Utils
 {
-    public class CornerQualityConverter : IValueConverter
+    public class MaskShapeConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is CornerQuality quality)
+            if (value is MaskShape shape)
             {
-                return quality switch
+                return shape switch
                 {
-                    CornerQuality.Low => "低质量",
-                    CornerQuality.Medium => "中等质量",
-                    CornerQuality.High => "高质量",
-                    _ => value.ToString() ?? string.Empty
+                    MaskShape.RoundedRectangle => "圆角矩形",
+                    MaskShape.Circle => "圆形",
+                    MaskShape.Ellipse => "椭圆",
+                    MaskShape.Polygon => "正多边形",
+                    _ => shape.ToString() ?? string.Empty
                 };
             }
+
             return string.Empty;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // 此转换器为单向（One-way），不支持反向转换
             throw new NotSupportedException();
         }
     }
@@ -63,6 +64,31 @@ namespace IcoConverter.Utils
             if (!int.TryParse(text, out var number))
             {
                 return new ValidationResult(false, "请输入整数");
+            }
+
+            if (number < Minimum || number > Maximum)
+            {
+                return new ValidationResult(false, $"范围 {Minimum}-{Maximum}");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+
+    /// <summary>
+    /// 浮点范围校验规则。
+    /// </summary>
+    public class DoubleRangeValidationRule : ValidationRule
+    {
+        public double Minimum { get; set; } = double.MinValue;
+        public double Maximum { get; set; } = double.MaxValue;
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var text = value?.ToString() ?? string.Empty;
+            if (!double.TryParse(text, NumberStyles.Float, cultureInfo, out var number))
+            {
+                return new ValidationResult(false, "请输入数字");
             }
 
             if (number < Minimum || number > Maximum)
