09397cfbc1134bc3e39fdf592a8e651011091e73
syj <falls_syj@qq.com>
2026-01-24 14:29:38 +0800

    Create MaskShape.cs

---
+++ src/Models/MaskShape.cs
@@ -0,0 +1,13 @@
+namespace IcoConverter.Models
+{
+    /// <summary>
+    /// 图像裁剪所支持的蒙版形状。
+    /// </summary>
+    public enum MaskShape
+    {
+        RoundedRectangle,
+        Circle,
+        Ellipse,
+        Polygon
+    }
+}
